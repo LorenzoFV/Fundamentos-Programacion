f65d70e60fb7ff291ffd680fc5a6d19be7e8e368
LorenzoFV <77753387+LorenzoFV@users.noreply.github.com>
2021-02-16 07:20:27 -0500

    Add files via upload

---
+++ Desafio 05-01.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Desafio_05_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            double totalEdades = 0;
+            int n;
+            int Contador = 0, max = 0, min = 100;
+
+            string NombreMayor = "nn";
+            string NombreMenor = "nn";
+            //Entrada
+            Console.Write("Ingrese el numero de datos (n) =");
+            n = int.Parse(Console.ReadLine());
+
+            while (Contador < n)
+            {
+                Console.Write("Digite el nombre:");
+                string Nombre = Console.ReadLine();
+                Console.Write("Digite la edad:");
+                int Edad = int.Parse(Console.ReadLine());
+
+                if (Edad > max)
+                {
+                    max = Edad;
+                    NombreMayor = Nombre;
+                }
+
+                if (Edad < min)
+                {
+                    min = Edad;
+                    NombreMenor = Nombre;
+                }
+
+
+                totalEdades += Edad;
+                Contador++;    
+
+            }
+            double Promedio = totalEdades / n;
+            Console.WriteLine("Promedio =" + Promedio);
+            Console.WriteLine("Mayor = " + max);
+            Console.WriteLine("Nombre del mayor = " + NombreMayor);
+            Console.WriteLine("Menor = " + min);
+            Console.WriteLine("Nombre del menor = " + NombreMenor);
+
+            
+
+
+
+        }
+    }
+}
---
+++ Desafio 05-02.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Desafio_05_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+
+            Random Numero = new Random();
+            string continuar = "s";
+            int dado1 = 0;
+            int dado2 = 0;
+            
+            int valortotal = dado1 + dado2;
+
+            int Seises = 0;
+            int Turnos = 0;
+            double PorcentajeSeis = 0;
+
+            int DoblesConsecutivos = 0;
+
+            //Entradas
+
+                      
+            while (continuar == "s" && (dado1 + dado2) != 2 && valortotal <100)
+            {
+                Turnos++;
+
+                dado1 = Numero.Next(1, 7);
+                dado2 = Numero.Next(1, 7);
+                valortotal += dado1 + dado2;
+                Console.WriteLine("El valor del dado 1 es: " + dado1);
+                Console.WriteLine("El valor del dado 2 es: " + dado2);
+
+                if(dado1 == dado2)
+                {
+                    DoblesConsecutivos++;
+                }
+
+                if((dado1 + dado2) > 6 )
+                {
+                    Seises++;
+                }
+
+                PorcentajeSeis = (100 * Seises) / Turnos;
+
+                if (valortotal > 99 || DoblesConsecutivos > 2)
+                {
+                    Console.WriteLine("El total es: " + valortotal);
+                    Console.WriteLine("El porcentaje de rondas donde sacó más de 6 es: " + PorcentajeSeis + "%");
+                    Console.WriteLine("Freiner, ganaste");
+                }
+
+                if ((dado1 + dado2) != 2 && valortotal < 100)
+                {
+                    Console.WriteLine("El total es: " + valortotal);
+                    Console.Write("Si desea seguir tirando presione (s) de lo contrario, presione (n): ");
+                    continuar = Console.ReadLine();
+                }
+
+
+            }
+
+            if(valortotal< 99)
+            {
+                Console.WriteLine("Hasta luego jon alexis");
+                Console.WriteLine("El porcentaje de turnos donde sacó más de 6 es: " + PorcentajeSeis + "%");
+            }
+
+            
+
+
+
+
+        }
+    }
+}
+
