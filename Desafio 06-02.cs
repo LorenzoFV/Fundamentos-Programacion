b8f2d719e75ccd2fb7c75bdce4db74222637076b
LorenzoFV <77753387+LorenzoFV@users.noreply.github.com>
2021-02-22 21:51:20 -0500

    Add files via upload

---
+++ Desafio 06-02.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Desafio_06_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+
+            double Seno = 0;
+
+            Console.WriteLine("Digite x:");
+
+            x = double.Parse(Console.ReadLine());
+
+            int n = 100;
+
+            for (int i = 0; i < n; i++)
+            {
+                Seno += ((Math.Pow(-1, i)) / (Factorial((2*i)+1))) * (Math.Pow(x,(2*i)+1));
+
+
+            }
+
+            Console.WriteLine(Seno);
+
+        }
+
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            return valor * Factorial(valor - 1);
+
+
+        }
+    }
+}
