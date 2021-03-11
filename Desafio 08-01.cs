ebb03fc90fa4dee8eebe730cd1a7b2c23d17786e
LorenzoFV <77753387+LorenzoFV@users.noreply.github.com>
2021-03-10 23:15:29 -0500

    Add files via upload

---
+++ Desafio 08-01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Desafio_08_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite una frase:");
+            string Entrada = Console.ReadLine();
+            Console.WriteLine("Digite una palabra:");
+            string Palabra = Console.ReadLine();
+
+            string EntradaMayus = Entrada.ToUpper();
+            string PalabraMayus = Palabra.ToUpper();
+                       
+            int ContadorPalabra = 0;
+
+            string[] Frase = EntradaMayus.Split();
+
+            for(int i= 0; i< Frase.Length; i++)
+            {
+                if(Frase[i] == PalabraMayus)
+                {
+                    Console.WriteLine("Palabra encontrada en la posición:" + (i + 1));
+                    ContadorPalabra++;
+                }
+
+               
+            }
+
+            Console.WriteLine("La palabra "+ PalabraMayus +" se encuentra " + ContadorPalabra + " veces.");
+
+        }
+    }
+}
